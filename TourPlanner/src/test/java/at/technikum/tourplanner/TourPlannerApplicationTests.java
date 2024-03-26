cea958f8bf80c36542f0753357929329f1ef78a7
Emil Auer <e.auer@itkaufmann.at>
2024-03-26 18:39:28 +0100

    Initialize project and folder structure

---
+++ TourPlanner/src/test/java/at/technikum/tourplanner/TourPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package at.technikum.tourplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
