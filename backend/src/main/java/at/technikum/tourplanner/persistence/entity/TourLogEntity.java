a5ee36dbc3aa101a4e4ed124e98d02daef77be4a
Emil Auer <e.auer@itkaufmann.at>
2024-04-01 20:09:50 +0200

    Add uml diagrams and github link

--- backend/src/main/java/at/technikum/tourplanner/persistence/entity/TourLogEntity.java
+++ backend/src/main/java/at/technikum/tourplanner/persistence/entity/TourLogEntity.java
@@ -20,21 +20,26 @@ public class TourLogEntity {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    @Column(nullable = false)
     @DateTimeFormat(pattern = "dd-MM-yyyy")
     private Date date;
 
+    @Column(nullable = false)
     private String comment;
 
+    @Column(nullable = false)
     private Integer difficulty;
 
-    @Column(name = "total_distance")
+    @Column(name = "total_distance", nullable = false)
     private Double totalDistance;
 
-    @Column(name = "total_time")
+    @Column(name = "total_time", nullable = false)
     private Double totalTime;
 
+    @Column(nullable = false)
     private Integer rating;
 
+    @Column(nullable = false)
     @ManyToOne(cascade = CascadeType.PERSIST)
     @JoinColumn(name = "tour_id")
     private TourEntity tour;
