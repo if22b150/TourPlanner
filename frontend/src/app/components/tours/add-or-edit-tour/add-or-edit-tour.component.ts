350edfd681e7a917e7aa80b6d24d2129c38b28fa
if23b073 <if23b073@technikum-wien.at>
2024-05-15 17:53:10 +0200

    Added add tour

--- frontend/src/app/components/tours/add-or-edit-tour/add-or-edit-tour.component.ts
+++ frontend/src/app/components/tours/add-or-edit-tour/add-or-edit-tour.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import {finalize} from "rxjs";
 import {NotificationService} from "../../../services/notification.service";
 import {FormBuilder, FormControl, ReactiveFormsModule, Validators} from "@angular/forms";
 import {DatePipe} from "@angular/common";
@@ -6,10 +7,13 @@ import {MdbModalRef} from "mdb-angular-ui-kit/modal";
 import {ModalComponent} from "../../utils/modal/modal.component";
 import {TourModel} from "../../../models/tour.model";
 import {FormInputComponent} from "../../utils/form-input/form-input.component";
+import { TourService } from 'src/app/services/tour.service';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-add-or-edit-tour',
   standalone: true,
+  //providers: [HeroService],
   imports: [
     ModalComponent,
     ReactiveFormsModule,
@@ -19,21 +23,32 @@ import {FormInputComponent} from "../../utils/form-input/form-input.component";
   styleUrl: './add-or-edit-tour.component.scss'
 })
 export class AddOrEditTourComponent {
-  tour: TourModel | null = null
+  @Input({required: true}) tour!: TourModel
+
   loading: boolean = false
 
   formGroup= this.formBuilder.group({
-    name: [null, Validators.required]
+    name: [null, Validators.required],
+    from: [null, Validators.required],
+    to: [null, Validators.required],
+    transportType: [null, Validators.required],
+    description: [null, Validators.required]
   })
 
   constructor(public modalRef: MdbModalRef<ModalComponent>,
               private datePipe: DatePipe,
               private notificationService: NotificationService,
-              private formBuilder: FormBuilder) {}
+              private formBuilder: FormBuilder,
+              private tourService: TourService
+            ) {}
 
   ngOnInit() {
     if (this.tour) {
       this.name?.setValue(this.tour.name);
+      this.from?.setValue(this.tour.from);
+      this.to?.setValue(this.tour.to);
+      this.transportType?.setValue(this.tour.transportType);
+      this.description?.setValue(this.tour.description)
     }
   }
 
@@ -44,29 +59,46 @@ export class AddOrEditTourComponent {
       return;
 
     let resource = this.formGroup.value
+    console.log(resource)
 
     this.loading = true
-    // this.customerService.create(resource)
-    //   .pipe(finalize(() => this.loading = false))
-    //   .subscribe({
-    //     next: (customer) => {
-    //       this.modalRef.close(customer)
-    //     },
-    //     error: (e: HttpErrorResponse) => {
-    //       console.log("Create customer error:")
-    //       console.log(e)
-    //
-    //       if (e?.error?.errors?.contractNumber == "The contract number has already been taken.")
-    //         this.customNotificationService.notify("The Contract Number already exists for another customer.", "warning", true, 4000)
-    //       else if (e?.error?.errors?.name == "The name has already been taken.")
-    //         this.customNotificationService.notify("The Name already exists for another customer.", "warning", true, 4000)
-    //       else
-    //         this.customNotificationService.notify("Customer could not be created.", "danger")
-    //     }
-    //   })
+    this.tourService.create(resource)
+      .pipe(finalize(() => this.loading = false))
+      .subscribe({
+        next: (tour) => {
+          this.modalRef.close(tour)
+        },
+        error: (e: HttpErrorResponse) => {
+          console.log("Create customer error:")
+          console.log(e)
+    
+          if (e?.error?.errors?.contractNumber == "The contract number has already been taken.")
+            this.notificationService.notify("The Contract Number already exists for another customer.", "warning", true, 4000)
+          else if (e?.error?.errors?.name == "The name has already been taken.")
+            this.notificationService.notify("The Name already exists for another customer.", "warning", true, 4000)
+          else
+            this.notificationService.notify("Tour konnte nicht erstellt werden", "danger")
+        }
+      })
   }
 
   get name(): FormControl | null {
     return this.formGroup.get('name') as FormControl
+  }
+
+  get from(): FormControl | null {
+    return this.formGroup.get('from') as FormControl
+  }
+
+  get to(): FormControl | null {
+    return this.formGroup.get('to') as FormControl
+  }
+
+  get transportType(): FormControl | null {
+    return this.formGroup.get('transportType') as FormControl
+  }
+
+  get description(): FormControl | null {
+    return this.formGroup.get('description') as FormControl
   }
 }
--- frontend/src/app/models/tour.model.ts
+++ frontend/src/app/models/tour.model.ts
@@ -1,5 +1,13 @@
 import {AModel} from "./a-model";
 
 export interface TourModel extends AModel {
-  name: string
+  id: number,
+  name: string,
+  description: string,
+  from: string,
+  to: string,
+  transportType: string,
+  distance: number,
+  estimatedTime: number,
+  imagePath: string
 }
