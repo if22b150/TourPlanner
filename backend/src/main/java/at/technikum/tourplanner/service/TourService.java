3abdee1b0098de6fb927f9bab97fd0394759f1b2
Emil Auer <e.auer@itkaufmann.at>
2024-06-01 18:06:55 +0200

    Clean up code

--- backend/src/main/java/at/technikum/tourplanner/service/TourService.java
+++ backend/src/main/java/at/technikum/tourplanner/service/TourService.java
@@ -2,7 +2,16 @@ package at.technikum.tourplanner.service;
 
 import at.technikum.tourplanner.persistence.entity.TourEntity;
 import at.technikum.tourplanner.service.dto.TourDto;
+import at.technikum.tourplanner.service.dto.TourLogDto;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.util.Date;
 import java.util.List;
 
 public interface TourService {
@@ -18,4 +27,19 @@ public interface TourService {
 
     void deleteTour(Long id);
 
+    ResponseEntity<InputStreamResource> generateTourReport(Long id);
+
+    ResponseEntity<InputStreamResource> generateToursSummaryReport();
+
+    void exportToursCsv(HttpServletResponse response) throws IOException;
+
+    void exportTourCsv(HttpServletResponse response, Long id) throws IOException;
+
+    void importToursCsv(MultipartFile file) throws IOException;
+
+    TourDto parseTourFromCsvLine(String line);
+
+    TourLogDto parseTourLogFromCsvLine(String line);
+
+    Date parseDate(String dateString);
 }
--- backend/src/main/java/at/technikum/tourplanner/service/impl/TourServiceImpl.java
+++ backend/src/main/java/at/technikum/tourplanner/service/impl/TourServiceImpl.java
@@ -1,21 +1,33 @@
 package at.technikum.tourplanner.service.impl;
 
+import at.technikum.tourplanner.persistence.entity.TourLogEntity;
 import at.technikum.tourplanner.persistence.repository.TourLogRepository;
 import at.technikum.tourplanner.service.MapApi;
+import at.technikum.tourplanner.service.TourLogService;
 import at.technikum.tourplanner.service.dto.TourDto;
 import at.technikum.tourplanner.persistence.entity.TourEntity;
+import at.technikum.tourplanner.service.dto.TourLogDto;
 import at.technikum.tourplanner.service.mapper.TourMapper;
 import at.technikum.tourplanner.persistence.repository.TourRepository;
 import at.technikum.tourplanner.service.TourService;
+import at.technikum.tourplanner.service.reports.PdfGenerator;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.util.Comparator;
-import java.util.List;
-import java.util.Optional;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 @Component
 @Transactional
@@ -28,6 +40,10 @@ public class TourServiceImpl implements TourService {
     private TourLogRepository tourLogRepository;
     @Autowired
     private MapApi mapApi;
+    @Autowired
+    private PdfGenerator pdfGenerator;
+    @Autowired
+    private TourLogService tourLogService;
 
     @Override
     public TourDto createTour(TourDto tourDto) {
@@ -110,5 +126,227 @@ public class TourServiceImpl implements TourService {
     @Override
     public List<TourEntity> getAllTourEntities() {
         return tourRepository.findAll();
+    }
+
+    @Override
+    public ResponseEntity<InputStreamResource> generateTourReport(Long id) {
+        try {
+            TourEntity tour = getTourById(id);
+            ByteArrayOutputStream pdfOutputStream = new ByteArrayOutputStream();
+            pdfGenerator.generateTourReport(tour, pdfOutputStream);
+
+            ByteArrayInputStream inputStream = new ByteArrayInputStream(pdfOutputStream.toByteArray());
+            InputStreamResource resource = new InputStreamResource(inputStream);
+            String filename = "tour_" + id + "_report.pdf";
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_PDF);
+            headers.setContentDispositionFormData("attachment", filename);
+
+            return ResponseEntity.ok()
+                    .headers(headers)
+                    .body(resource);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    @Override
+    public ResponseEntity<InputStreamResource> generateToursSummaryReport() {
+        try {
+            List<TourEntity> tours = getAllTourEntities();
+            ByteArrayOutputStream pdfOutputStream = new ByteArrayOutputStream();
+            pdfGenerator.generateToursSummaryReport(tours, pdfOutputStream);
+
+            ByteArrayInputStream inputStream = new ByteArrayInputStream(pdfOutputStream.toByteArray());
+            InputStreamResource resource = new InputStreamResource(inputStream);
+            String filename = "tours_summary_report.pdf";
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_PDF);
+            headers.setContentDispositionFormData("attachment", filename);
+
+            return ResponseEntity.ok()
+                    .headers(headers)
+                    .body(resource);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    @Override
+    public void exportToursCsv(HttpServletResponse response) throws IOException {
+        // Set response content type
+        response.setContentType("text/csv");
+
+        // Set response header for file attachment
+        response.setHeader("Content-Disposition", "attachment; filename=\"tours.csv\"");
+
+        response.setCharacterEncoding("UTF-8");
+        // Get PrintWriter for writing CSV content
+        PrintWriter writer =  new PrintWriter(new OutputStreamWriter(response.getOutputStream(), StandardCharsets.UTF_8));
+
+        // Fetch all tours from the repository
+        List<TourEntity> tours = this.getAllTourEntities();
+
+        // Write tours data
+        for (TourEntity tour : tours) {
+            // Write tours section header
+            writer.println("Tour");
+            writer.println(
+                    tour.getName() + ";;;" +
+                            tour.getDescription() + ";;;" +
+                            tour.getFrom() + ";;;" +
+                            tour.getTo() + ";;;" +
+                            tour.getTransportType() + ";;;" +
+                            tour.getDistance() + ";;;" + // Convert meters to kilometers
+                            tour.getEstimatedTime() + ";;;" + // Convert seconds to hours
+                            tour.getImagePath()
+            );
+
+            // Write tour logs for the current tour
+            writer.println("TourLogs");
+            for (TourLogEntity log : tour.getTourLogs()) {
+                writer.println(
+                        log.getDate() + ";;;" +
+                                log.getComment() + ";;;" +
+                                log.getDifficulty() + ";;;" +
+                                log.getTotalDistance() + ";;;" + // Convert meters to kilometers
+                                log.getTotalTime() + ";;;" + // Convert seconds to hours
+                                log.getRating()
+                );
+            }
+        }
+
+        // Flush and close the writer
+        writer.flush();
+        writer.close();
+    }
+
+    @Override
+    public void exportTourCsv(HttpServletResponse response, Long id) throws IOException {
+        TourEntity tour = this.getTourById(id);
+
+        // Set response content type
+        response.setContentType("text/csv");
+
+        // Set response header for file attachment
+        response.setHeader("Content-Disposition", "attachment; filename=\"tour_" + tour.getId() + ".csv\"");
+
+        response.setCharacterEncoding("UTF-8");
+        // Get PrintWriter for writing CSV content
+        PrintWriter writer =  new PrintWriter(new OutputStreamWriter(response.getOutputStream(), StandardCharsets.UTF_8));
+
+        // Write tours section header
+        writer.println("Tour");
+        writer.println(
+                tour.getName() + ";;;" +
+                        tour.getDescription() + ";;;" +
+                        tour.getFrom() + ";;;" +
+                        tour.getTo() + ";;;" +
+                        tour.getTransportType() + ";;;" +
+                        tour.getDistance() + ";;;" + // Convert meters to kilometers
+                        tour.getEstimatedTime() + ";;;" + // Convert seconds to hours
+                        tour.getImagePath()
+        );
+
+        // Write tour logs for the current tour
+        writer.println("TourLogs");
+        for (TourLogEntity log : tour.getTourLogs()) {
+            writer.println(
+                    log.getDate() + ";;;" +
+                            log.getComment() + ";;;" +
+                            log.getDifficulty() + ";;;" +
+                            log.getTotalDistance() + ";;;" + // Convert meters to kilometers
+                            log.getTotalTime() + ";;;" + // Convert seconds to hours
+                            log.getRating()
+            );
+        }
+
+        // Flush and close the writer
+        writer.flush();
+        writer.close();
+    }
+
+    @Override
+    public void importToursCsv(MultipartFile file) throws IOException {
+        if (file.isEmpty()) {
+            throw new IllegalArgumentException("File is empty");
+        }
+
+        // Read the CSV file using BufferedReader
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(file.getInputStream(), StandardCharsets.UTF_8))) {
+            String line;
+            List<TourDto> tours = new ArrayList<>();
+            List<TourLogDto> tourLogs = new ArrayList<>();
+            boolean isTourSection = false;
+            Long currentTourId = null;
+
+            while ((line = reader.readLine()) != null) {
+                if (line.equals("Tour")) {
+                    isTourSection = true;
+                    continue;
+                } else if (line.equals("TourLogs")) {
+                    isTourSection = false;
+                    continue;
+                }
+
+                if (isTourSection) {
+                    // Parse tour data from CSV line and create a TourDto object
+                    TourDto tourDto = parseTourFromCsvLine(line);
+                    TourDto created = this.createTour(tourDto);
+                    currentTourId = created.getId();
+                } else {
+                    // Parse tour log data from CSV line and create a TourLogDto object
+                    TourLogDto tourLogDto = parseTourLogFromCsvLine(line);
+                    tourLogService.createTourLog(tourLogDto, currentTourId);
+                }
+            }
+        }
+    }
+
+    @Override
+    public TourDto parseTourFromCsvLine(String line) {
+        String[] parts = line.split(";;;");
+
+        TourDto tourDto = new TourDto();
+        tourDto.setName(parts[0]);
+        tourDto.setDescription(parts[1]);
+        tourDto.setFrom(parts[2]);
+        tourDto.setTo(parts[3]);
+        tourDto.setTransportType(parts[4]);
+        tourDto.setDistance(Double.parseDouble(parts[5]) * 1000); // Convert kilometers to meters
+        tourDto.setEstimatedTime(Double.parseDouble(parts[6]) * 3600); // Convert hours to seconds
+        tourDto.setImagePath(parts[7]);
+
+        return tourDto;
+    }
+
+    @Override
+    public TourLogDto parseTourLogFromCsvLine(String line) {
+        String[] parts = line.split(";;;");
+
+        TourLogDto tourLogDto = new TourLogDto();
+        tourLogDto.setDate(parseDate(parts[0]));
+        tourLogDto.setComment(parts[1]);
+        tourLogDto.setDifficulty(Integer.parseInt(parts[2]));
+        tourLogDto.setTotalDistance(Double.parseDouble(parts[3])); // Convert kilometers to meters
+        tourLogDto.setTotalTime(Double.parseDouble(parts[4])); // Convert hours to seconds
+        tourLogDto.setRating(Integer.parseInt(parts[5]));
+
+        return tourLogDto;
+    }
+
+    @Override
+    public Date parseDate(String dateString) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd"); // Adjust the format according to your date string format
+        try {
+            return dateFormat.parse(dateString);
+        } catch (ParseException e) {
+            // Handle parsing exception
+            return null;
+        }
     }
 }
